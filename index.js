e7a89078add402dd8d345bbd9a0ddd858241a6bd
Noah Fairbairn <noahfairbairn@gmail.com>
2019-06-24 15:30:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Your code here
+
+var dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = '#FF69B4';
+
+dodger.style.bottom = "0px";
+
+function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+    // debugger;
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+    // debugger;
+   
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+document.addEventListener("keydown", function(e) {
+    // debugger;
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+      }
+    
+  });
+
+//   document.addEventListener("keydown", function(e) {
+//     if (e.key === "ArrowRight") {
+//         moveDodgerRight();
+//       }
+//   });
